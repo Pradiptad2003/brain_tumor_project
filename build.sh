fca8ea0be2c7f37bb1fffba892437b8ff39de915
Pradipta Das <pradiptad23@gmail.com>
2026-04-25 01:31:10 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,6 +3,9 @@
 echo "Installing dependencies..."
 pip install -r requirements.txt
 
+echo "Downloading model..."
+wget "https://drive.google.com/uc?id=1oZzSuFrAKoOPE3PHUAwOM7JyUGRxZhw1" -O model/brain_model.h5
+
 echo "Applying migrations..."
 python manage.py migrate
 
