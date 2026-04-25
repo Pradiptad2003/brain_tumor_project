f5c52336cdc6e2452f04df971c2033fc00492b7b
Pradipta Das <pradiptad23@gmail.com>
2026-04-25 11:05:36 +0530

    fix model download link

--- build.sh
+++ build.sh
@@ -1,13 +1,9 @@
 #!/usr/bin/env bash
 
-echo "Installing dependencies..."
 pip install -r requirements.txt
 
-echo "Downloading model..."
-wget "https://drive.google.com/uc?id=1oZzSuFrAKoOPE3PHUAwOM7JyUGRxZhw1" -O model/brain_model.h5
-
-echo "Applying migrations..."
-python manage.py migrate
-
-echo "Collecting static files..."
 python manage.py collectstatic --noinput
+
+mkdir -p model
+
+wget "https://drive.google.com/uc?export=download&id=1oZzSuFrAKoOPE3PHUAwOM7JyUGRxZhw1" -O model/brain_model.h5
