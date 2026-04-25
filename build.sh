9ca3308492ba8955932d2d0fd5f442c8d511cd17
Pradipta Das <pradiptad23@gmail.com>
2026-04-25 11:13:37 +0530

    fix large model download

--- build.sh
+++ build.sh
@@ -6,4 +6,5 @@ python manage.py collectstatic --noinput
 
 mkdir -p model
 
-wget "https://drive.google.com/uc?export=download&id=1oZzSuFrAKoOPE3PHUAwOM7JyUGRxZhw1" -O model/brain_model.h5
+# fix large file download (IMPORTANT)
+gdown --id 1oZzSuFrAKoOPE3PHUAwOM7JyUGRxZhw1 -O model/brain_model.h5
